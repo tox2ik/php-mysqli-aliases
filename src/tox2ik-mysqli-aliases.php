05e2457c516b12e07ce40293d9b37ed4a4712012
jaroslav <jazzoslav@gmail.com>
2017-10-27 11:43:54 +0200

    add getPrettyTrace

--- src/tox2ik-mysqli-aliases.php
+++ src/tox2ik-mysqli-aliases.php
@@ -206,3 +206,26 @@ function qError() {
     $db = db();
     return $db->error;
 }
+
+if (! function_exists('getPrettyTrace')) {
+	function getPrettyTrace() {
+		$trace ='';
+		$btrace = debug_backtrace(
+			DEBUG_BACKTRACE_PROVIDE_OBJECT |
+			DEBUG_BACKTRACE_IGNORE_ARGS );
+
+		foreach ($btrace as $element) {
+			if ( !isset($element['file'] ))
+				$element['file'] = '*nofile*';
+
+			if ( !isset($element['line'] ))
+				$element['line'] = -1;
+
+			$trace .= sprintf("%20s/%20s %4d %20s()\n",
+				dirname( basename( $element['file'])),
+				basename( $element['file']) ,
+				$element['line'], $element['function']);
+		}
+		return $trace;
+	}
+}
